bb9f416a80c3006d80f1e9845e53c02524a023ff
Arman <armandwi.pangestu7@gmail.com>
2024-07-07 03:38:25 +0700

    slicing: Checkout Page

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
 
 Route::get('/login', function () {
     return view('login');
 })->name('login');
+
+Route::get('/checkout', function () {
+    return view('checkout');
+})->name('checkout');
