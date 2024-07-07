74d404ef37d6a85f2b4d583a2b073a0c1f20ab03
Arman <armandwi.pangestu7@gmail.com>
2024-07-07 09:27:40 +0700

    oauth: install breeze

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,9 @@ Route::get('/checkout', function () {
 Route::get('/success-checkout', function () {
     return view('success_checkout');
 })->name('success-checkout');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__ . '/auth.php';
