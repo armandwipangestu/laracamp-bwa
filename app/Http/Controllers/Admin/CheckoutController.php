d03f6ab12425232f0425e62e00bac600f6e42faa
Arman <armandwi.pangestu7@gmail.com>
2024-07-31 11:43:44 +0700

    Admin: Email Paid

--- app/Http/Controllers/Admin/CheckoutController.php
+++ app/Http/Controllers/Admin/CheckoutController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Mail\Checkout\Paid;
 use App\Models\Checkout;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class CheckoutController extends Controller
 {
@@ -12,7 +14,12 @@ class CheckoutController extends Controller
     {
         $checkout->is_paid = true;
         $checkout->save();
+
+        // sending email to user
+        Mail::to($checkout->User->email)->send(new Paid($checkout));
+
         $request->session()->flash('success', "Checkout with ID {$checkout->id} has been updated");
+
 
         return redirect(route('admin.dashboard'));
     }
